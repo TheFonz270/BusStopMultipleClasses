5a2d53c215a7c7ee11d04cbbfbfc952de3e9d3ad
Martin Quinn <thefonz270@hotmail.com>
2021-10-26 13:54:27 +0100

    all tests passed

--- bus_lab_start_code/src/bus_stop.py
+++ bus_lab_start_code/src/bus_stop.py
@@ -1,8 +1,13 @@
 class BusStop:
     def __init__(self, bus_stop):
-        self.bus_stop = bus_stop
+        self.name = bus_stop
         self.queue = []
 
     def add_to_queue(self, person):
         self.queue.append(person)
 
+    def queue_length(self):
+        return len(self.queue)
+
+    def clear(self):
+        self.queue = []
